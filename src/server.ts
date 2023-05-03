66f2008de4b7990bc9424683b90186d0e8eab142
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-05-02 23:49:12 -0300

    ✨ feat: Add prisma client and connection check

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import { app } from './app';
+import { prisma } from './database';
 import { env } from './config/env.config';
 
 const port = env.PORT || 3333;
 
-app.listen(port, () => console.log(`🚀 Server running at port ${port}.`));
+async function main() {
+    try {
+        await prisma.$connect();
+        console.info('✅ Successfully connected to PostgreSQL database.');
+
+        app.listen(port, () => console.log(`🚀 WaiterAPP 2.0 API running at port ${port}.`));
+    } catch (err) {
+        console.error('❌ Error connecting to database:');
+        console.error(err);
+        console.error('❌ Error connecting to database.');
+    }
+}
+
+main();
