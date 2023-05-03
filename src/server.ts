6bbd036eb55cb7665971919e63dba98b394c7f9d
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-05-03 00:23:43 -0300

    ✨ feat: Use connectDatabase() to verify connection with postgres

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import { app } from './app';
-import { prisma } from './database';
+import { connectDatabase } from './database';
 import { env } from './config/env.config';
 
 const port = env.PORT || 3333;
 
 async function main() {
   try {
-    await prisma.$connect();
-
+    await connectDatabase();
 
     app.listen(port, () => console.log(`🚀 WaiterAPP 2.0 API running at port ${port} in "${env.NODE_ENV}" mode.`));
   } catch (err) {
