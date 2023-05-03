491bbca01021ffadb5f0c3c87b95cf6275da1e6c
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-05-02 23:49:42 -0300

    🌈 style: Reident code style to 2 spaces

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,16 @@ import { env } from './config/env.config';
 const port = env.PORT || 3333;
 
 async function main() {
-    try {
-        await prisma.$connect();
-        console.info('✅ Successfully connected to PostgreSQL database.');
+  try {
+    await prisma.$connect();
+    console.info('✅ Successfully connected to PostgreSQL database.');
 
-        app.listen(port, () => console.log(`🚀 WaiterAPP 2.0 API running at port ${port}.`));
-    } catch (err) {
-        console.error('❌ Error connecting to database:');
-        console.error(err);
-        console.error('❌ Error connecting to database.');
-    }
+    app.listen(port, () => console.log(`🚀 WaiterAPP 2.0 API running at port ${port}.`));
+  } catch (err) {
+    console.error('❌ Error connecting to database:');
+    console.error(err);
+    console.error('❌ Error connecting to database.');
+  }
 }
 
 main();
